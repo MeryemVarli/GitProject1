28baa2b11a9b04563e4e06591c9cfcc299ad7302
meryemvarli <meryemvarli@gmail.com>
2024-08-05 20:39:48 -0400

    for branch

--- src/project1GitHub.java
+++ src/project1GitHub.java
@@ -2,7 +2,7 @@ public class project1GitHub {
     public static void main(String[] args){
 
 
-        System.out.println("Project git");
+
         System.out.println("something for new branch git");
         System.out.println("same fornew baarnch");
 
